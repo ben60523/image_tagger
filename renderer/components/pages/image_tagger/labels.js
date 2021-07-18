ddc05f9b15668392e4b657e9603c8664a2f2bc29
Owen Chang <owen.chang@fasprosystems.com>
2021-07-18 17:53:35 +0800

    Recactory: clean the label

--- renderer/components/pages/image_tagger/labels.js
+++ renderer/components/pages/image_tagger/labels.js
@@ -1,78 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react';
 
 import ContextStore from '../../../context_store';
-// import EditBar from './edit_bar';
-
-import { addNewTaggingLabel } from '../../../reducers/label_actions';
-import defaultabel from '../../../reducers/default_label';
-
-// const PENCIL = 'pencil';
-const AUTO_GENERATE = 'auto_gen';
 
 export default ({ setTagConfig }) => {
-  // const [enteredLabel, setEnteredLabel] = useState(null);
-  const { labels, ldispatch, projectName } = useContext(ContextStore);
+  const { labels } = useContext(ContextStore);
   const [focusedLabel, setFocusLabel] = useState(labels[0]);
-  const [currentInput, setCurrentInput] = useState('');
-  const [editedLabel] = useState(null);
 
   const updateCurrentLabel = (selectedLabel) => {
-    if (editedLabel === null && selectedLabel.describe !== AUTO_GENERATE) {
-      setTagConfig(selectedLabel);
-      setFocusLabel(selectedLabel);
-    }
+    setTagConfig(selectedLabel);
+    setFocusLabel(selectedLabel);
   };
 
-  // const saveLabel = (e, selectedLabel) => {
-  //   if (e.keyCode === 13) {
-  //     if (editedLabel !== null && currentInput.length !== 0) {
-  //       ldispatch(updateLabel(selectedLabel, { title: currentInput }));
-  //     }
-  //     setEditedLabel(null);
-  //   }
-  // };
-
-  // const editLabel = (e, selectedLabel) => {
-  //   setCurrentInput(selectedLabel.title);
-  //   if (e.target.className.includes(PENCIL)) {
-  //     setEditedLabel(selectedLabel.key);
-  //   } else if (e.button === 2) setEditedLabel(selectedLabel.key);
-  //   else if (editedLabel !== selectedLabel.key) {
-  //     setEditedLabel(null);
-  //   }
-  // };
-
-  // const getEditBar = (label) => {
-  //   if (enteredLabel && editedLabel === null) {
-  //     return enteredLabel.key === label.key
-  //       ? <EditBar name={PENCIL} />
-  //       : null;
-  //   }
-
-  //   return null;
-  // };
-
-  const taggingLabelFilter = (label) => label.type === 'tagging';
-
-  const projectFilter = (label) => (
-    label.project === 'default' || label.project === projectName
-  );
-
   useEffect(() => {
-    setTagConfig(focusedLabel);
-  }, [focusedLabel]);
-
-  useEffect(() => {
-    if (labels.filter(taggingLabelFilter).length !== 0) {
-      if (labels[0].describe !== AUTO_GENERATE) {
-        setFocusLabel(labels[0]);
-      } else {
-        setFocusLabel(labels[1]);
-      }
-    } else {
-      const action = addNewTaggingLabel(defaultabel);
-      setFocusLabel(action.payload[0]);
-      ldispatch(action);
+    if (labels.length !== 0) {
+      setFocusLabel(labels[0]);
     }
   }, []);
 
@@ -83,8 +24,6 @@ export default ({ setTagConfig }) => {
       </h5>
       {
         labels.length !== 0 ? labels
-          .filter(taggingLabelFilter)
-          .filter(projectFilter)
           .sort((a, b) => (a.title > b.title ? 1 : -1))
           .map((label) => (
             <div
@@ -99,9 +38,6 @@ export default ({ setTagConfig }) => {
                 alignItems: 'center',
               }}
               onClick={() => updateCurrentLabel(label)}
-              // onMouseDown={(e) => editLabel(e, label)}
-              // onMouseEnter={() => setEnteredLabel(label)}
-              // onMouseLeave={() => setEnteredLabel(null)}
               onKeyDown={() => console.log('key down')}
               tabIndex={0}
             >
@@ -109,21 +45,7 @@ export default ({ setTagConfig }) => {
                 className={`icon ${focusedLabel.key !== label.key ? 'icon-record' : 'icon-play'}`}
                 style={{ color: label.color, marginRight: '5px' }}
               />
-              {
-                editedLabel === label.key
-                  ? (
-                    <input
-                      className="form-control"
-                      style={{ padding: '3px 7px' }}
-                      value={currentInput}
-                      onChange={(e) => setCurrentInput(e.target.value)}
-                      type="text"
-                      placeholder={label.title}
-                    />
-                  )
-                  : label.title
-              }
-              {/* { getEditBar(label) } */}
+              {label.title}
             </div>
           )) : null
       }
