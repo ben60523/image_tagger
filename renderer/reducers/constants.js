ef621d43c2094f9d987274b16390fd706506bc76
Owen Chang <owen.chang@fasprosystems.com>
2021-07-18 21:02:18 +0800

    Remove close page function

--- renderer/reducers/constants.js
+++ renderer/reducers/constants.js
@@ -1,5 +1,4 @@
 export const ADD_PAGE = 'ADD_PAGE';
-export const CLOSE_PAGE = 'CLOSE_PAGE';
 export const UPDATE_LABEL = 'UPDATE_LABEL';
 export const ADD_NEW_LABEL = 'ADD_NEW_LABEL';
 export const INITIALIZE_LABEL = 'INITIALIZE_LABEL';
