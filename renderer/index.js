0fb97802f9a2125982357732bb63929445ac94f4
Owen Chang <owen.chang@fasprosystems.com>
2021-07-27 17:16:37 +0800

    Fix: fixed babel async issue

--- renderer/index.js
+++ renderer/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import { HashRouter as Router } from 'react-router-dom';
 import Wrapper from './containers/wrapper';
 
