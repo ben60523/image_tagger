a8b7b21b9177804cc0be5ffe66e9913fd5aa38fb
Owen Chang <owenchang.dev@gmail.com>
2021-09-17 14:36:44 +0800

    Feat: check label before adding to the list

--- renderer/reducers/label_reducer.js
+++ renderer/reducers/label_reducer.js
@@ -6,9 +6,52 @@ const getNewLabelList = (labels, newLabel) => {
   return [...labels];
 };
 
-const addNewLabel = (preLabels, label) => (
-  [...preLabels, ...label].sort((a, b) => (a.title > b.title ? 1 : -1))
-);
+const addNewLabel = (preLabels, newlabels) => {
+  const isValidColor = (strColor) => {
+    const s = new Option().style;
+    s.color = strColor;
+
+    // return 'false' if color wasn't assigned
+    return s.color !== '';
+  };
+
+  const isString = (title) => typeof title === 'string';
+
+  const titlehasExisted = (newlabel) => {
+    const index = preLabels.findIndex((label) => newlabel.title === label.title);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const checkNewLabel = () => {
+    const indexOfInValidLabel = newlabels.findIndex((label) => {
+      if (
+        isString(label.title)
+        && isValidColor(label.color)
+        && !titlehasExisted(label)
+      ) {
+        return false;
+      }
+      return true;
+    });
+
+    if (indexOfInValidLabel !== -1) {
+      return false;
+    }
+
+    return true;
+  };
+
+  if (checkNewLabel()) {
+    return [...preLabels, ...newlabels].sort((a, b) => (a.title > b.title ? 1 : -1));
+  }
+
+  return preLabels;
+};
 
 export default (state, action) => {
   switch (action.type) {
